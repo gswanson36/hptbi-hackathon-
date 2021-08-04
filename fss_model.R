aa54f78af62c62a222b242d455d58eb04eb7a50f
Glenn Swanson <87099640+gswanson36@users.noreply.github.com>
2021-08-04 12:20:43 -0600

    v0.4
    update fss_model

--- fss_model.R
+++ fss_model.R
@@ -52,6 +52,14 @@ predict.hackathon_fss_model <- function(object, newdata, ...) {
   # user defined code starts here
 
   as.integer(stats::predict.lm(object, newdata, type = "response"))
+  
+  ifelse(as.integer(stats::predict.lm(object, newdata, type = "response")) < 6,
+         yes = 6,
+         no = as.integer(stats::predict.lm(object, newdata, type = "response")))
+  
+  ifelse(as.integer(stats::predict.lm(object, newdata, type = "response")) > 30,
+         yes = 30,
+         no = as.integer(stats::predict.lm(object, newdata, type = "response")))
 
 }
  
